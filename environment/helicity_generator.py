- Faster calculation of the mass dimension and little group scaling used at data generation - Correcting the sign of the little group scaling - Adding the option for either BatchNorm or LayerNorm in the projection head of the contrastive model - When generating examples with the transformer model add a flag to explicitly only retain the last word (bug fix) - Adding the option to track both the mass dimension and the little group scaling during training

environment/helicity_generator.py
@@ -7,7 +7,8 @@ import random
 from itertools import combinations
 from environment.bracket_env import ab, sb
 from add_ons.mathematica_utils import solve_diophantine_systems
-from environment.utils import reorder_expr, generate_random_bk, get_scaling_expr_detail, build_scale_factor
+from environment.utils import (reorder_expr, generate_random_bk, get_scaling_expr_detail, build_scale_factor,
+                               get_expression_detail_lg_scaling)
 from sympy import latex, Function, sympify, fraction, cancel
 from logging import getLogger
 import sys
@@ -188,7 +189,7 @@ def generate_random_amplitude(npt_list, rng=None, max_terms_scale=1, max_compone
                                                           rng, canonical_form=canonical_form,
                                                           zero_allowed=components == 1)
     denominator = fraction(return_expr)[-1]
-    scaling_list = get_scaling_expr_detail(return_expr, [ab, sb], n_points)
+    scaling_list = get_expression_detail_lg_scaling(return_expr, [ab, sb], n_points)
     if info_scaling:
         scale_list = np.array(scaling_list[0]) - np.array(scaling_list[1])
 

environment/spin_helicity_env.py
@@ -6,7 +6,7 @@ import numpy as np
 import random, time
 import sympy as sp
 from environment.utils import reorder_expr, generate_random_bk, get_scaling_expr,\
-    random_scale_factor, get_scaling_expr_detail, build_scale_factor
+    random_scale_factor, get_scaling_expr_detail, build_scale_factor, get_expression_detail_lg_scaling
 from sympy import Function, latex
 from environment.helicity_generator import generate_random_fraction_unbounded
 from add_ons.mathematica_utils import solve_diophantine_system
@@ -260,8 +260,8 @@ class SpinHelExpr:
                                                               canonical_form=canonical, zero_allowed=False)
         else:
             # Get the scaling necessary to correct it
-            num_scales, denom_scales = get_scaling_expr_detail(add_expr_in, [ab, sb], self.n_point)
-            bk_scales, _ = get_scaling_expr_detail(bk_base, [ab, sb], self.n_point)
+            num_scales, denom_scales = get_expression_detail_lg_scaling(add_expr_in, [ab, sb], self.n_point)
+            bk_scales, _ = get_expression_detail_lg_scaling(bk_base, [ab, sb], self.n_point)
             num_scales = np.array(num_scales) - np.array(bk_scales)
 
             coeff_add_num = solve_diophantine_system(self.n_point, num_scales, session)

model/contrastive_learner.py
@@ -13,11 +13,12 @@ logger = getLogger()
 
 class FFNHead(nn.Module):
 
-    def __init__(self, embed_dim, layer_nums):
+    def __init__(self, embed_dim, layer_nums, norm=None):
         super().__init__()
         self.activation = nn.ReLU()
         self.embed_dim = embed_dim
         self.layer_nums = layer_nums
+        self.norm_layer = norm
 
         layer_list = []
 
@@ -25,6 +26,10 @@ class FFNHead(nn.Module):
             layer_list.append(('layer_%d' % (i + 1), nn.Linear(self.embed_dim, self.embed_dim)))
 
             if i + 1 < self.layer_nums:
+                if self.norm_layer == 'batchnorm':
+                    layer_list.append(nn.BatchNorm1d(self.embed_dim))
+                elif self.norm_layer == 'layernorm':
+                    layer_list.append(nn.LayerNorm(self.embed_dim, eps=1e-12))
                 layer_list.append(('activation_%d' % (i + 1), self.activation))
 
         self.model = nn.Sequential(OrderedDict(layer_list))
@@ -41,7 +46,7 @@ class TransformerEncoderC(TransformerModel):
         Transformer model (encoder or decoder).
         """
         super().__init__(params, id2word, True, False)
-        self.ffnhead = FFNHead(params.emb_dim, params.head_layers)
+        self.ffnhead = FFNHead(params.emb_dim, params.head_layers, params.norm_ffn)
 
     def forward(self, mode, **kwargs):
         """

model/transformer.py
@@ -340,7 +340,7 @@ class TransformerModel(nn.Module):
         loss = F.cross_entropy(scores, y, reduction='mean')
         return scores, loss
 
-    def generate(self, src_enc, src_len, max_len=200, sample_temperature=None):
+    def generate(self, src_enc, src_len, max_len=200, sample_temperature=None, last_word=False):
         """
         Decode a sentence given initial start.
         `x`:
@@ -352,6 +352,7 @@ class TransformerModel(nn.Module):
         `positions`:
             - False, for regular "arange" positions (LM)
             - True, to reset positions from the new generation (MT)
+        'last_word': explicitly only retain the last word predicton (when cache is not updated)
         """
 
         # input batch
@@ -388,6 +389,8 @@ class TransformerModel(nn.Module):
                 src_len=src_len,
                 cache=cache
             )
+            if last_word:
+                tensor = tensor[-1::]
             assert tensor.size() == (1, bs, self.dim), (cur_len, max_len, src_enc.size(), tensor.size(), (1, bs, self.dim))
             tensor = tensor.data[-1, :, :].type_as(src_enc)  # (bs, dim)
             scores = self.proj(tensor)                       # (bs, n_words)

training/evaluator.py
@@ -13,7 +13,7 @@ import sympy as sp
 
 from add_ons.mathematica_utils import sp_to_mma, check_numerical_equiv_mma
 from add_ons.numerical_evaluations import check_numerical_equiv_local
-from environment.utils import to_cuda, timeout, TimeoutError
+from environment.utils import to_cuda, timeout, TimeoutError, get_expression_lg_scaling, convert_sp_forms
 from environment.char_env import InvalidPrefixExpression
 
 logger = getLogger()
@@ -201,6 +201,8 @@ class Evaluator(object):
 
         # stats
         xe_loss = 0
+        valid_m_scalings = 0
+        valid_lg_scalings = 0
         n_valid = torch.zeros(2000, dtype=torch.long)
         n_total = torch.zeros(2000, dtype=torch.long)
 
@@ -239,6 +241,36 @@ class Evaluator(object):
             t[pred_mask] += word_scores.max(1)[1] == y
             valid = (t.sum(0) == len2 - 1).cpu().long()
 
+            if params.scaling_eval:
+                for i in range(len(len1)):
+                    if not valid[i]:
+                        tgt_sp = idx_to_sp(env, x2[1:len2[i] - 1, i].tolist())
+                        tgt_scalings = get_expression_lg_scaling(convert_sp_forms(tgt_sp, env.func_dict),
+                                                                 list(env.func_dict.values()), max(env.npt_list))
+                        if not params.cpu:
+                            encoded = encoded.type(torch.float16)
+                        greedy_sol, _ = decoder.generate(encoded[:len1[i:i+1], i:i+1, :].transpose(0, 1),
+                                                         src_len=len1[i:i+1], max_len=params.max_len,
+                                                         sample_temperature=None, last_word=params.scaling_eval)
+                        try:
+                            pred_sp = idx_to_sp(env, greedy_sol[1:- 1][:, 0].tolist())
+                        except RecursionError:
+                            pred_sp = None
+                        # If we have a valid prediction we look at its scaling and compare it to the target
+                        if pred_sp is not None:
+                            try:
+                                pred_scalings = get_expression_lg_scaling(convert_sp_forms(pred_sp, env.func_dict),
+                                                                          list(env.func_dict.values()), max(env.npt_list))
+                                valid_m_scalings += tgt_scalings[0] == pred_scalings[0]
+                                valid_lg_scalings += all(tgt_scalings[1:] == pred_scalings[1:])
+                            except:
+                                print(greedy_sol[1:- 1][:, 0].tolist())
+                                print(pred_sp)
+                                print(convert_sp_forms(pred_sp, env.func_dict))
+                    else:
+                        valid_m_scalings += 1
+                        valid_lg_scalings += 1
+
             # export evaluation details
             if params.eval_verbose:
                 for i in range(len(len1)):
@@ -269,11 +301,13 @@ class Evaluator(object):
         scores[f'{data_type}_{task}_xe_loss'] = xe_loss / _n_total
         scores[f'{data_type}_{task}_acc'] = 100. * _n_valid / _n_total
 
+        if params.scaling_eval:
+            scores[f'{data_type}_{task}_m_scaling'] = 100. * valid_m_scalings / _n_total
+            scores[f'{data_type}_{task}_lg_scaling'] = 100. * valid_lg_scalings / _n_total
         # per class perplexity and prediction accuracy
         for i in range(len(n_total)):
             if n_total[i].item() == 0:
                 continue
-            # logger.info(f"{i}: {n_valid[i].item()} / {n_total[i].item()} ({100. * n_valid[i].item() / max(n_total[i].item(), 1)}%)")
             scores[f'{data_type}_{task}_acc_{i}'] = 100. * n_valid[i].item() / max(n_total[i].item(), 1)
 
     def enc_dec_step_beam(self, data_type, task, scores):